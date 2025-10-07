326d2de0aa1a3a9adee8408f434f2788106d962c
Jannis Morgenstern <49486580+nyarthan@users.noreply.github.com>
2025-10-07 19:31:41 +0000

    ci: nixify ci setup (#33)

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "cypress-iframe";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-parts.url = "github:hercules-ci/flake-parts/main";
   };
 
   outputs =
@@ -13,24 +13,66 @@
     }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "aarch64-darwin"
-        "x86_64-darwin"
+	"x86_64-linux"
+	"aarch64-linux"
+	"aarch64-darwin"
+	"x86_64-darwin"
       ];
 
       perSystem =
-        {
-          pkgs,
-          ...
-        }:
-        {
-          devShells.default = pkgs.mkShell {
-            packages = [
-              pkgs.nodejs_22
-              pkgs.pnpm_10
-            ];
-          };
-        };
+	{
+	  pkgs,
+	  ...
+	}:
+	let
+	  nodejs = pkgs.nodejs_24;
+	  pnpm = pkgs.pnpm.override { inherit nodejs; };
+	in
+	{
+	  formatter = pkgs.nixfmt-rfc-style;
+
+	  apps.write-versions = {
+	    type = "app";
+	    program = pkgs.callPackage ./nix/write-versions.nix {
+	      nodejsVersion = nodejs.version;
+	      pnpmVersion = pnpm.version;
+	    };
+	    meta.description = "Writes the versions of Node.js & pnpm used by nix to package.json.";
+	  };
+
+	  checks.check-versions = pkgs.callPackage ./nix/check-versions.nix {
+	    nodejsVersion = nodejs.version;
+	    pnpmVersion = pnpm.version;
+	  };
+
+	  devShells =
+	    let
+	      runtimePackages = [
+		nodejs
+		pnpm
+	      ];
+	      devtoolPackages = [
+		pkgs.lefthook
+		pkgs.nixfmt-rfc-style
+	      ];
+
+	      local = pkgs.mkShell {
+		packages = runtimePackages ++ devtoolPackages;
+
+		shellHook = ''
+		  lefthook install
+		'';
+	      };
+
+	      ci = pkgs.mkShell {
+		packages = runtimePackages;
+	      };
+
+	      default = local;
+	    in
+	    {
+	      inherit default local ci;
+	    };
+	};
     };
 }
